3846b81197302e2f3d524ef438260e44f3087873
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-09-17 18:42:04 -0500

    Coalescent & null condition operators implementation

---
+++ Overview/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
+
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> results = new List<string>();
+
+            foreach (Product product in Product.GetProducts())
+            {
+                string name = product?.Name ?? "<No Name>"; //combining conditional and coalescent
+                decimal? price = product?.Price ?? 0;
+
+                string relatedName = product?.Related?.Name ?? "<None>"; // detecting null chain
+
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+            }
+            return View(results);
+        }
+    }
+}
---
+++ Overview/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public Product Related { get; set; } // chaining null operator
+
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product
+            {
+                Name = "Kayak",
+                Price = 274M
+            };
+
+            Product lifejacket = new Product
+            {
+                Name = "LifeJacket",
+                Price = 43.98M
+            };
+
+            kayak.Related = lifejacket;
+
+            return new Product[] { kayak, lifejacket, null }; 
+        }
+
+    }
+}

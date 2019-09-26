5d1d8afa5af22d30b51a7f0d71ec888a959d21dd
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-09-26 15:01:29 -0500

    Working with Razor

---
+++ Overview/Razor/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product kayak = new Product
+            {
+                Name = "Kayak",
+                Price = 275M,
+                Description = "Boat for one person",
+                ProductID = 1,
+                Category = "WaterSports"
+            };
+
+            ViewBag.StockLevel = 5;
+
+            return View(kayak);
+        }
+    }
+}

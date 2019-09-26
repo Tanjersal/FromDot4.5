bffbf00b5e58bf3db176e432685883229fa88f6b
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-09-26 14:00:12 -0500

    Async/Await

--- Overview/LanguageFeatures/Controllers/HomeController.cs
+++ Overview/LanguageFeatures/Controllers/HomeController.cs
@@ -9,36 +9,43 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        //public IActionResult Index()
+        //{
+        //    //lambda func
+        //    bool FilterByPriceUsingLambda(Product p)
+        //    {
+        //        return ((p?.Price ?? 0) >= 20);
+        //    }
+
+        //    ShoppingCart cart = new ShoppingCart()
+        //    {
+        //        Products = Product.GetProducts()
+        //    };
+
+        //    Product[] productArray =
+        //    {
+        //        new Product {Name = "Kayak", Price = 120M},
+        //        new Product {Name = "Lifejacket", Price = 32.44M},
+        //        new Product { Name = "Toyota Rav4", Price = 900.40M}
+        //    };
+
+        //    decimal totalProductArray = productArray.TotalPrice(); // extension applies to array list
+        //    decimal totalCart = cart.TotalPrice();
+
+        //    decimal lambdaTotal = productArray.FilterByPriceLambda(FilterByPriceUsingLambda).TotalPrice(); //using lambda 
+
+        //    decimal yieldedTotalPrice = productArray.FilterByPrice(700).TotalPrice();
+
+        //    return View("Index", new string[] { $"Total filter: {yieldedTotalPrice:C2}, Lambda total: {lambdaTotal:C2}"});
+
+        //    //return View("Index", new string[] { $"Cart Total: {totalCart:C2}", $"Array Total: {totalProductArray:C2}"});
+        ////}
+
+        public async Task<ViewResult> Index()
         {
-            //lambda func
-            bool FilterByPriceUsingLambda(Product p)
-            {
-                return ((p?.Price ?? 0) >= 20);
-            }
+            long? httpCallLength = await AsyncMethods.GetPageLength();
 
-            ShoppingCart cart = new ShoppingCart()
-            {
-                Products = Product.GetProducts()
-            };
-
-            Product[] productArray =
-            {
-                new Product {Name = "Kayak", Price = 120M},
-                new Product {Name = "Lifejacket", Price = 32.44M},
-                new Product { Name = "Toyota Rav4", Price = 900.40M}
-            };
-
-            decimal totalProductArray = productArray.TotalPrice(); // extension applies to array list
-            decimal totalCart = cart.TotalPrice();
-
-            decimal lambdaTotal = productArray.FilterByPriceLambda(FilterByPriceUsingLambda).TotalPrice(); //using lambda 
-
-            decimal yieldedTotalPrice = productArray.FilterByPrice(700).TotalPrice();
-
-            return View("Index", new string[] { $"Total filter: {yieldedTotalPrice:C2}, Lambda total: {lambdaTotal:C2}"});
-
-            //return View("Index", new string[] { $"Cart Total: {totalCart:C2}", $"Array Total: {totalProductArray:C2}"});
+            return View("Index", new string[] { $"length from http call: {httpCallLength}" });
         }
     }
 }

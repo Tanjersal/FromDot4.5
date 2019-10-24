97d5ffe6f22463f58205b2c4433ee41009c01fd5
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-10-24 15:21:44 -0500

    setup Mock for isolating unit test interfaces

--- Overview/UnitTesting.Tests/HomeControllerTest.cs
+++ Overview/UnitTesting.Tests/HomeControllerTest.cs
@@ -5,6 +5,8 @@ using System.Text;
 using UnitTesting.Controllers;
 using UnitTesting.Models;
 using Xunit;
+using Moq;
+
 
 namespace UnitTesting.Tests
 {
@@ -48,7 +50,8 @@ namespace UnitTesting.Tests
         [Theory]
         [InlineData(275, 48.95, 19.50, 24.95)]
         [InlineData(5, 8.95, 19.50, 24.95)]
-        public void IndexActionModelIsCompleteParameterize(decimal price1, decimal price2, decimal price3, decimal price4)
+        public void IndexActionModelIsCompleteParameterize(
+            decimal price1, decimal price2, decimal price3, decimal price4)
         {
             //arrange
             var controller = new HomeController();
@@ -70,6 +73,73 @@ namespace UnitTesting.Tests
             //assert
             Assert.Equal(controller.repository.Products, model,
                 Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+
+        [Theory]
+        [ClassData(typeof(ProductTestData))]
+        public void IndexActionModelIsCompleteParametrizeWithMethod(Product[] products)
+        {
+            //arrange
+            var controller = new HomeController();
+            controller.repository = new ModelCompleteFakeRepository()
+            {
+                Products = products
+            };
+
+            //act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            //assert
+            Assert.Equal(controller.repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+
+        }
+
+        /// <summary>
+        /// Isolated ModelCompleteFakeRepository all together
+        /// </summary>
+        /// <param name="products"></param>
+        [Theory]
+        [ClassData(typeof(ProductTestData))]
+        public void IndexActionModelIsCompleteUsingMoqFramework(Product[] products)
+        {
+            // Arrange
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(x => x.Products).Returns(products);
+            var controller = new HomeController()
+            {
+                repository = mock.Object //instance of the mock repository
+            };
+
+
+            //act
+            var model = (controller.Index() as ViewResult)?.Model as IEnumerable<Product>;
+
+            //assert
+            Assert.Equal(controller.repository.Products, model,
+                Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+
+
+        /// <summary>
+        /// using moq to check this
+        /// </summary>
+        /// 
+        [Fact]
+        public void RepositoryCalledOnce()
+        {
+            //arrange
+            var moq = new Mock<IRepository>();
+            moq.SetupGet(x => x.Products).Returns(new[] { new Product { Name = "P1", Price=10 } });
+            var controller = new HomeController()
+            {
+                repository = moq.Object
+            };
+
+            //act
+            var result = controller.Index();
+
+            //assert - using verify if a prop was read on the moq obj
+            moq.VerifyGet(x => x.Products, Times.Once);
         }
     }
 }
---
+++ Overview/UnitTesting/Models/ProductTestData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnitTesting.Models;
+
+namespace UnitTesting.Models
+{
+    public class ProductTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { GetPriceUnder50() };
+            yield return new object[] { GetPriceOver50 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Method - get product under 50
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<Product> GetPriceUnder50()
+        {
+            decimal[] prices = new decimal[] { 275, 48.95M, 19.50M, 24.95M };
+            for (int i = 0; i < prices.Length; i++)
+            {
+                yield return new Product { Name = $"P{i + 1}", Price = prices[i] };
+            }
+        }
+
+        /// <summary>
+        /// Proprety - get product over 50
+        /// </summary>
+        private Product[] GetPriceOver50 => new Product[]
+        {
+            new Product { Name="P1", Price = 275},
+            new Product { Name = "P2", Price = 48.95M},
+            new Product { Name = "P3", Price = 19.50M},
+            new Product { Name = "P4", Price = 24.95M}
+        };
+    }
+}

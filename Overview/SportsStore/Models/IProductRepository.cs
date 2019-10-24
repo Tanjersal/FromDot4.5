24b6f84e663a7df2d3b39e84170602e1e4dc35c7
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-10-24 16:34:22 -0500

    new product model and repositories, and registered repository model

---
+++ Overview/SportsStore/Models/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public interface IProductRepository
+    {
+        //allows for better db queries (always convert to toList or 
+       // toArray avoid multiple db calls) vs IEnumerable
+
+        IQueryable<Product> Products { get; } 
+    }
+}

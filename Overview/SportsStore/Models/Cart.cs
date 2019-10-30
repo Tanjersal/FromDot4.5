fb337cb79c567901c07b5c1b2afcb45b4c01a906
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-10-30 16:04:01 -0500

    CartLine - new model w functions - unit testing module

---
+++ Overview/SportsStore/Models/Cart.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class Cart
+    {
+        private List<CartLine> cartLines = new List<CartLine>(); //cartLines
+
+        /// <summary>
+        /// Add an item to the line
+        /// </summary>
+        /// <param name="quantity"></param>
+        /// <param name="product"></param>
+        public virtual void AddItem(int quantity, Product product)
+        {
+            CartLine line = cartLines.Where(x => x.Product.ProductID == product.ProductID).FirstOrDefault();
+
+            if (line == null)
+            {
+                cartLines.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+                line.Quantity++;
+            
+        }
+
+        /// <summary>
+        /// Removes a line in the cart
+        /// </summary>
+        /// <param name="product"></param>
+        public virtual void RemoveLineItem(Product product)
+        {
+            cartLines.RemoveAll(x => x.Product.ProductID == product.ProductID);
+        }
+
+
+        /// <summary>
+        /// Compute the total in the cart
+        /// </summary>
+        /// <returns></returns>
+        public virtual decimal ComputeTotalCart()
+        {
+            return cartLines.Sum(x => x.Product.Price * x.Quantity);
+        }
+
+        /// <summary>
+        /// Clears the cart
+        /// </summary>
+        public virtual void ClearLine()
+        {
+            cartLines.Clear();
+        }
+
+        /// <summary>
+        /// Return the cartLine
+        /// </summary>
+        public IEnumerable<CartLine> GetCartLines => cartLines;
+
+
+        /// <summary>
+        /// cartLine model
+        /// </summary>
+        public class CartLine
+        {
+            public int CartLineID { get; set; }
+            public Product Product { get; set; }
+            public int Quantity { get; set; }
+        }
+    }
+}
---
+++ Overview/SportsStoreTests/CartLineTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using SportsStore.Models;
+using static SportsStore.Models.Cart;
+using System.Linq;
+
+namespace SportsStoreTests
+{
+    public class CartLineTest
+    {
+        [Fact]
+        public void Can_AddItem_Cart()
+        {
+            //arrange
+            Product testProduct = new Product()
+            {
+                Name = "Orange",
+                Price = 901.87M,
+                Category = "Testing"
+            };
+
+            //act
+            Cart testCart = new Cart();
+            testCart.AddItem(2, testProduct);
+
+            CartLine[] cartLines = testCart.GetCartLines.ToArray();
+
+            //assert
+            Assert.Single(cartLines);
+            
+        }
+    }
+}

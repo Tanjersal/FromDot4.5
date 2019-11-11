457eabdd66db86d1ed24493d543f4fc5a3448f90
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-11-11 17:19:55 -0600

    Cart Summary - completed view component - added fontawesome dependencies

---
+++ Overview/SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent: ViewComponent
+    {
+        private Cart cartService; // DI
+
+        public CartSummaryViewComponent(Cart cart)
+        {
+            cartService = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cartService);
+        }
+    }
+}

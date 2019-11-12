d933148dcd10810bea64bc4fa0e4d02098bb62e1
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-11-11 20:08:12 -0600

    Order - created db context - migrations - registered service

---
+++ Overview/SportsStore/Models/EFOrderRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        private ApplicationDbContext dbContext;
+
+        public EFOrderRepository(ApplicationDbContext applicationDbContext)
+        {
+            dbContext = applicationDbContext;
+        }
+
+        /// <summary>
+        /// Get orders - all lines and product
+        /// </summary>
+        public IQueryable<Order> Orders => dbContext.Orders.Include(o => o.Lines).ThenInclude(l => l.Product);
+
+        /// <summary>
+        /// Saving order
+        /// </summary>
+        /// <param name="order">new order</param>
+        public void SaveOrders(Order order)
+        {
+            dbContext.AttachRange(order.Lines.Select(l => l.Product));
+
+            if(order.OrderID == 0)
+            {
+                dbContext.Orders.Add(order);
+            }
+
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Overview/SportsStore/Models/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public interface IOrderRepository
+    {
+        IQueryable<Order> Orders { get; }
+        void SaveOrders(Order order);
+    }
+}

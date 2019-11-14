15cce6028345d435ae405ee2e02ae9f414c47231
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-11-14 15:41:06 -0600

    Creating and registering a singleton servie

--- Modules/Configuring/Controllers/HomeController.cs
+++ Modules/Configuring/Controllers/HomeController.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Configuring.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Configuring.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() 
-            => View(new Dictionary<string, string> { ["message"] = "This is the index action" });
+        private UptimeService uptimeService;
+
+        public HomeController(UptimeService uptime)
+        {
+            uptimeService = uptime;
+        }
+
+        public ViewResult Index()
+        {
+            return View(new Dictionary<string, string>
+            {
+                ["Message"] = "This is the index action",
+                ["Uptime"] = $"{uptimeService.Uptime}ms"
+            });
+        }
     }
 }
---
+++ Modules/Configuring/Infrastructure/UptimeService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Configuring.Infrastructure
+{
+    public class UptimeService
+    {
+        private Stopwatch timer;
+
+        public UptimeService()
+        {
+            timer = new Stopwatch();
+        }
+
+        /// <summary>
+        /// Display uptime in milliseconds
+        /// </summary>
+        public long Uptime => timer.ElapsedMilliseconds;
+    }
+}

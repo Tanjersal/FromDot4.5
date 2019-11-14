634535e0db60456ad285fbdfd3d3f3707b88fa84
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-11-14 16:50:04 -0600

    Manipulating request middleware type

---
+++ Modules/Configuring/Infrastructure/BrowserTypeMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Configuring.Infrastructure
+{
+    public class BrowserTypeMiddleware
+    {
+        private RequestDelegate RequestDelegate;
+
+        public BrowserTypeMiddleware(RequestDelegate next)
+        {
+            RequestDelegate = next;
+        }
+
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            //Dict Items can be used to share data in current context
+            httpContext.Items["EdgeBrowser"] = httpContext.Request.Headers["User-Agent"]
+                .Any(x => x.ToLower().Contains("edge"));
+
+            await RequestDelegate.Invoke(httpContext);
+        }
+
+    }
+}
--- Modules/Configuring/Infrastructure/ShortCircuitMiddleware.cs
+++ Modules/Configuring/Infrastructure/ShortCircuitMiddleware.cs
@@ -17,8 +17,7 @@ namespace Configuring.Infrastructure
 
         public async Task Invoke(HttpContext httpContext)
         {
-            if (httpContext.Request.Headers["User-Agent"]
-                .Any(x => x.ToLower().Contains("edge")))
+            if (httpContext.Items["EdgeBrowser"] as bool? == true)
             {
                 httpContext.Response.StatusCode = 403; //terminate request
             }

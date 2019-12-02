c2d1dfe64586c248d590eb12c1e94d64a0e08f61
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-12-01 19:11:37 -0600

    Routes and Urls project

---
+++ Modules/UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Index controller - prints request info
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View("Result", new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(Index)
+            });
+        }
+    }
+}

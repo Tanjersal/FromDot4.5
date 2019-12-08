5a1ae8478ef310b5b4193511df9f7df2ca909546
Fabien Thiombiano <fabienthiombiano@gmail.com>
2019-12-08 00:37:19 -0600

    Advances routings - setting routes

--- Modules/UrlsAndRoutes/Startup.cs
+++ Modules/UrlsAndRoutes/Startup.cs
@@ -33,7 +33,13 @@ namespace UrlsAndRoutes
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
